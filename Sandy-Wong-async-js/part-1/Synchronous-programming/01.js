912e036dfe3a0ccb56e3ff351abde76fba804129
SandyWong2024 <157861548+SandyWong2024@users.noreply.github.com>
2024-10-12 20:05:28 -0700

    Add files via upload

---
+++ Sandy-Wong-async-js/part-1/Synchronous-programming/01.js
@@ -0,0 +1,17 @@
+//Synchronous Function
+
+//Example 1
+/*const name = "Sandy";
+const greeting =`Hello, my name is ${name}!`;
+console.log(greeting);*/
+// "Hello, my name is Sandy!"
+
+//Example 2
+function makeGreeting(name){
+    return `Hello, my name is ${name}!`;
+}
+
+const name = "Sandy";
+const greeting =`Hello, my name is ${name}!`;
+console.log(greeting);
+// "Hello, my name is Sandy!"
